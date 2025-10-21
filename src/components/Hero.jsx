bfa459ff73263ad6993c945d915a94536d5c0a04
Abhishek Gupta <amansg710@gmail.com>
2025-10-21 12:23:37 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ export default function Hero() {
     <section className="relative w-full h-screen flex items-center justify-center overflow-hidden">
       {/* 3D Background */}
       <CarScene />
-      <div className="absolute bottom-8 right-8 text-white opacity-80 text-right z-10">
-        <h2 className="text-8xl font-Pilowalava mb-2" style={{ fontFamily: 'Pilowlava, sans-serif' }}>Welcome To Luxury</h2>
+      <div className="absolute text-white opacity-60 text-center bottom-5 z-10">
+        <h2 className="text-8xl font-Pilowalava mb-2 text-center" style={{ fontFamily: 'Pilowlava, sans-serif'}}>Welcome To Luxury</h2>
       </div>
     </section>
   );
